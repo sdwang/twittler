e84e02a69319a9613bb849cf985bc8d2eed2e8ed
Si <siwang22@gmail.com>
2015-09-01 18:08:28 -0700

    Refactored code and removed repetitive code.  Created updateFeed function.

--- application.js
+++ application.js
@@ -5,26 +5,25 @@ $(document).ready(function(){
         //var index = streams.home.length - 1;
         var index = 0;
         var streamsLength = streams.home.length - 1;
-        
-        while(index <= streamsLength){
-          var tweet = streams.home[index];
-          var $tweet = $('<div></div>');
-          $tweet.text('@' + tweet.user + ': ' + tweet.message + tweet.created_at);
-          $tweet.appendTo($feed);
-          index += 1;
-        }
+
+        var updateFeed = function() {
+            while(index <= streamsLength){
+            var tweet = streams.home[index];
+            var $tweet = $('<div></div>');
+            $tweet.text('@' + tweet.user + ': ' + tweet.message + tweet.created_at);
+            $tweet.prependTo($feed);
+            index += 1;
+          }
+        };
+
+        //Initial update of feed right after the DOM loads
+        updateFeed();
 
         //Listens for button event. Prepends new tweets to the feed
         $('button').on('click', function() {
           if(streams.home.length - 1 > index) {
             streamsLength = streams.home.length - 1;
-            while(index <= streamsLength) {
-              var tweet = streams.home[index];
-              var $tweet = $('<div></div>');
-              $tweet.text('@' + tweet.user + ': ' + tweet.message + tweet.created_at);
-              $tweet.prependTo($feed);
-              index += 1;
-            }
+            updateFeed();
           }
         })
 
