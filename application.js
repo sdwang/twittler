b12a8183b6759827363215eb4ed72fc2f24e9135
Si <siwang22@gmail.com>
2015-09-01 17:08:45 -0700

    <body> no longer updated to display feed.  Instead, a new section shows the feed

--- application.js
+++ application.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
-        var $body = $('body');
-        $body.html('');
+        var $feed = $('.feed');
+        $feed.html('');
 
         var index = streams.home.length - 1;
         while(index >= 0){
           var tweet = streams.home[index];
           var $tweet = $('<div></div>');
           $tweet.text('@' + tweet.user + ': ' + tweet.message);
-          $tweet.appendTo($body);
+          $tweet.appendTo($feed);
           index -= 1;
         }
 
