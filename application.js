2758149ad93d7da3800f7f07e950d6f044289924
Si <siwang22@gmail.com>
2015-09-01 17:53:03 -0700

    Refactored initial feed so that index keeps track of how many tweets are currently in the feed

--- application.js
+++ application.js
@@ -2,13 +2,22 @@ $(document).ready(function(){
         var $feed = $('.feed');
         $feed.html('');
 
-        var index = streams.home.length - 1;
-        while(index >= 0){
+        //var index = streams.home.length - 1;
+        var index = 0;
+        var streamsLength = streams.home.length - 1;
+        while(index <= streamsLength){
           var tweet = streams.home[index];
           var $tweet = $('<div></div>');
           $tweet.text('@' + tweet.user + ': ' + tweet.message + tweet.created_at);
           $tweet.appendTo($feed);
-          index -= 1;
+          index += 1;
         }
+
+        //Listens for button event. Prepends new tweets to the feed
+        $('button').on('click', function() {
+          if(streams.home.length - 1 > index) {
+          }
+        })
+
 
       });
